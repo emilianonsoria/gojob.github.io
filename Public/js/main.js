bca08588909a629d0b7ba357d93aacec8361199b
emilianonsoria <emilianonsoria@gmail.com>
2023-04-19 15:51:39 -0300

    falta footer

--- Public/js/main.js
+++ Public/js/main.js
@@ -8,3 +8,49 @@ function fixNav() {
         nav.classList.remove('active')
     }
 }
+
+
+
+const testimonialsContainer = document.querySelector('.testimonials-container')
+const testimonial = document.querySelector('.testimonial')
+const userImage = document.querySelector('.user-image')
+const username = document.querySelector('.username')
+const role = document.querySelector('.role')
+
+const testimonials = [
+  {
+    name: 'Emiliano Fernandez',
+    position: 'CEO',
+    photo:
+    '/Asset/img/perfil.png',
+    text:
+    "Tras 15 años de experiencia en el ámbito informático y luego de haber co-creado WIZTECH, PolygonTech y varios proyectos chicos, decido aventurarme nuevamente en la creación de una nueva marca donde pueda volver a destacarme por lo que mejor se hacer y donde pueda ser yo mismo enteramente.",
+  },
+  {
+    name: 'Emiliano Soria',
+    position: 'Web Developer',
+    photo:
+      '/Asset/img/perfil.png',
+    text:
+    "Diseñamos espacios de trabajo limpios, rápidos, estéticos y eficientes. Para impulsar su negocio con todo el potencial del Marketing Digital.",
+  },
+]
+
+let idx = 1
+
+function updateTestimonial() {
+  const { name, position, photo, text } = testimonials[idx]
+
+  testimonial.innerHTML = text
+  userImage.src = photo
+  username.innerHTML = name
+  role.innerHTML = position
+
+  idx++
+
+  if (idx > testimonials.length - 1) {
+    idx = 0
+  }
+}
+
+setInterval(updateTestimonial, 10000)
